480d5b07321d70b7295ce146bdbb260f00c43f5d
Evgeny Yagrushkin <yagrushkin@gmail.com>
2019-05-31 20:34:40 -0700

    Initial Commit

---
+++ ResumeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResumeDemo
+//
+//  Created by Evgeny Yagrushkin on 2019-05-31.
+//  Copyright © 2019 Evgeny Yagrushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ResumeDemoTests/ResumeDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ResumeDemoTests.swift
+//  ResumeDemoTests
+//
+//  Created by Evgeny Yagrushkin on 2019-05-31.
+//  Copyright © 2019 Evgeny Yagrushkin. All rights reserved.
+//
+
+import XCTest
+@testable import ResumeDemo
+
+class ResumeDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
